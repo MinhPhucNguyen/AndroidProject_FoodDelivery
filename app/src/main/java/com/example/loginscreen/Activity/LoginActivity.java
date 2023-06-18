49380589493b63b836d381302694be52b86968fd
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2023-06-18 13:19:44 +0700

    Up code

--- app/src/main/java/com/example/loginscreen/Activity/LoginActivity.java
+++ app/src/main/java/com/example/loginscreen/Activity/LoginActivity.java
@@ -4,54 +4,150 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.loginscreen.DBHelper.DBHelper;
 import com.example.loginscreen.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView loginBtn, signUpNowBtn, signupBtn, loginNowBtn, backHomeBtn;
+    TextView loginBtn, loginNowBtn, signUpNowBtn, signupBtn2, signUpBtn, backHomeBtn;
 
     LinearLayout signUpLayout, loginLayout;
+
+    ImageView eye, eye2;
+
+    EditText txtUsername, txtPassword;
+
+    EditText txtUsernameRegister,
+            txtPhoneNumberRegister,
+            txtAddressRegister,
+            txtPasswordRegister;
+
+    Boolean passwordVisible = false;
+
+    Boolean isOpenLoginFrom = true;
+
+    DBHelper dbHelper = new DBHelper(LoginActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        //LOGIN
         loginNowBtn = (TextView) findViewById(R.id.loginNowBtn);
         loginBtn = (TextView) findViewById(R.id.loginBtn);
         loginLayout = (LinearLayout) findViewById(R.id.loginLayout);
+        eye = (ImageView) findViewById(R.id.eye);
+        eye2 = (ImageView) findViewById(R.id.eye2);
+
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+        eye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!passwordVisible) {
+                    txtPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordVisible = true;
+                } else {
+                    txtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordVisible = false;
+                }
+            }
+        });
+
+        eye2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!passwordVisible) {
+                    txtPasswordRegister.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordVisible = true;
+                } else {
+                    txtPasswordRegister.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordVisible = false;
+                }
+            }
+        });
+
+        //SIGN UP
         signUpNowBtn = (TextView) findViewById(R.id.signUpNowBtn);
-        signupBtn = (TextView) findViewById(R.id.signupBtn);
+        signupBtn2 = (TextView) findViewById(R.id.signupBtn2);
+        signUpBtn = (TextView) findViewById(R.id.signUpBtn);
         signUpLayout = (LinearLayout) findViewById(R.id.signUpLayout);
+
+        txtUsernameRegister = (EditText) findViewById(R.id.txtUsernameRegister);
+        txtPhoneNumberRegister = (EditText) findViewById(R.id.txtPhoneNumberRegister);
+        txtAddressRegister = (EditText) findViewById(R.id.txtAddressRegister);
+        txtPasswordRegister = (EditText) findViewById(R.id.txtPasswordRegister);
+
         backHomeBtn = (TextView) findViewById(R.id.backHomeBtn);
+
+        View.OnClickListener editTextClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isOpenLoginFrom) {
+                    isOpenLoginFrom = false;
+                    signUpLayout.setVisibility(View.VISIBLE);
+                    signUpLayout.animate()
+                            .translationY(0)
+                            .alpha(1.0f)
+                            .setDuration(500)
+                            .setListener(null);
+                }
+            }
+        };
+
+        txtUsernameRegister.setOnClickListener(editTextClickListener);
+        txtPhoneNumberRegister.setOnClickListener(editTextClickListener);
+        txtAddressRegister.setOnClickListener(editTextClickListener);
+        txtPasswordRegister.setOnClickListener(editTextClickListener);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String username = txtUsername.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                if (isValidLoginForm(username, password)) {
+                    if (dbHelper.checkLogin(username, password)) {
+                        Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login failed. Please try again", Toast.LENGTH_SHORT).show();
+                    }
+
+                    dbHelper.close();
+                }
             }
         });
-
 
         View.OnClickListener loginClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 backHomeBtn.setText("");
-                signupBtn.setText("Sign Up");
+                signupBtn2.setText("Sign Up");
                 signUpLayout.setVisibility(View.GONE);
                 loginLayout.setVisibility(View.VISIBLE);
                 Animation slideUpAnimation = new TranslateAnimation(0, 0, loginLayout.getHeight(), 0);
                 slideUpAnimation.setDuration(300);
                 loginLayout.startAnimation(slideUpAnimation);
+                isOpenLoginFrom = true;
             }
         };
         loginNowBtn.setOnClickListener(loginClickListener);
@@ -59,7 +155,7 @@ public class LoginActivity extends AppCompatActivity {
         backHomeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(backHomeBtn.getText() == "Login"){
+                if (backHomeBtn.getText() == "Login") {
                     backHomeBtn.setOnClickListener(loginClickListener);
                 }
             }
@@ -72,7 +168,8 @@ public class LoginActivity extends AppCompatActivity {
                     signUpLayout.setVisibility(View.VISIBLE);
                     loginLayout.setVisibility(View.GONE);
                     backHomeBtn.setText("Login");
-                    signupBtn.setText("");
+                    signupBtn2.setText("");
+                    isOpenLoginFrom = false;
 
                     signUpLayout.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
                         @Override
@@ -94,11 +191,12 @@ public class LoginActivity extends AppCompatActivity {
 
                 } else {
                     backHomeBtn.setText("");
-                    signupBtn.setText("Sign Up");
+                    signupBtn2.setText("Sign Up");
                     Animation slideDownAnimation = new TranslateAnimation(0, 0, 0, signUpLayout.getHeight());
                     slideDownAnimation.setDuration(300);
                     signUpLayout.startAnimation(slideDownAnimation);
                     loginLayout.startAnimation(slideDownAnimation);
+                    isOpenLoginFrom = false;
                     slideDownAnimation.setAnimationListener(new Animation.AnimationListener() {
                         @Override
                         public void onAnimationStart(Animation animation) {
@@ -108,7 +206,6 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onAnimationEnd(Animation animation) {
                             signUpLayout.setVisibility(View.GONE);
-                            loginLayout.setVisibility(View.VISIBLE);
                         }
 
                         @Override
@@ -121,7 +218,76 @@ public class LoginActivity extends AppCompatActivity {
         };
 
         signUpNowBtn.setOnClickListener(signUpClickListener);
-        signupBtn.setOnClickListener(signUpClickListener);
+        signupBtn2.setOnClickListener(signUpClickListener);
 
+
+        signUpLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isOpenLoginFrom) {
+                    signUpLayout.setVisibility(View.VISIBLE);
+                    isOpenLoginFrom = false;
+                }
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = txtUsernameRegister.getText().toString().trim();
+                String phone_number = txtPhoneNumberRegister.getText().toString().trim();
+                String address = txtAddressRegister.getText().toString().trim();
+                String password = txtPasswordRegister.getText().toString().trim();
+
+                register(username, phone_number, address, password);
+            }
+        });
+
+    }
+
+    private boolean isValidRegister(String username, String phone_number, String address, String password) {
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (TextUtils.isEmpty(phone_number)) {
+            Toast.makeText(this, "Please enter a phone number", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (TextUtils.isEmpty(address)) {
+            Toast.makeText(this, "Please enter a address", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void register(String username, String phone_number, String address, String password) {
+        if (isValidRegister(username, phone_number, address, password)) {
+            boolean insert = dbHelper.insertUser(username, phone_number, address, password);
+            if (insert) {
+                Toast.makeText(this, "Sign Up Successfully", Toast.LENGTH_SHORT).show();
+                if(loginLayout.getVisibility() == View.GONE){
+                    loginLayout.setVisibility(View.VISIBLE);
+                }
+            } else {
+                Toast.makeText(this, "Sign up failed. Please try again", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean isValidLoginForm(String username, String password) {
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/loginscreen/DBHelper/DBHelper.java
+++ app/src/main/java/com/example/loginscreen/DBHelper/DBHelper.java
@@ -1,33 +1,65 @@
-//package com.example.loginscreen.DBHelper;
-//
-//import android.content.Context;
-//import android.database.sqlite.SQLiteDatabase;
-//import android.database.sqlite.SQLiteOpenHelper;
-//
-//import androidx.annotation.Nullable;
-//
-//public class DBHelper extends SQLiteOpenHelper {
-//
-//    public static final String DATABASE_NAME = "orderfoodapp_db";
-//
-//    public static final String TABLE_NAME = "ORDER";
-//
-//    private SQLiteDatabase sqLiteDatabase;
-//
-////    private static  final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (fooo_id INTEGER NOT NULL, quantity INTEGER NOT NULL);";
-//
-//    public DBHelper(Context context) {
-//        super(context, DATABASE_NAME, null, 1);
-//    }
-//
-//    @Override
-//    public void onCreate(SQLiteDatabase db) {
-//        db.execSQL(CREATE_TABLE);
-//    }
-//
-//    @Override
-//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL("DROP TABLE IF EXISTS " +  DATABASE_NAME);
-//        onCreate(db);
-//    }
-//}
+package com.example.loginscreen.DBHelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "food_app.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "users";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_PHONE_NUMBER = "phone_number";
+    public static final String COLUMN_ADDRESS = "address";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_ROLE_AS = "role_as";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create_users_table = "CREATE TABLE " + TABLE_NAME + " ( " + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ," + COLUMN_USERNAME + " TEXT, " + COLUMN_PHONE_NUMBER + " TEXT, " + COLUMN_ADDRESS + " TEXT," + COLUMN_PASSWORD + " TEXT, " + COLUMN_ROLE_AS + " INTEGER DEFAULT 0)";
+        db.execSQL(create_users_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertUser(String username, String phone, String address, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_USERNAME, username);
+        contentValues.put(COLUMN_PHONE_NUMBER, phone);
+        contentValues.put(COLUMN_ADDRESS, address);
+        contentValues.put(COLUMN_PASSWORD, password);
+        db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+        return true;
+    }
+
+    public boolean checkLogin(String username, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {COLUMN_USERNAME};
+        String selection = COLUMN_USERNAME + " = ? AND " + COLUMN_PASSWORD + " = ?";
+        String[] selectionArgs = {username, password};
+        Cursor cursor = db.query(TABLE_NAME, columns, selection, selectionArgs, null, null, null);
+        boolean hasMatch = (cursor != null && cursor.moveToFirst());
+        if (cursor != null) {
+            cursor.close();
+        }
+        db.close();
+        return hasMatch;
+    }
+}
